522d60e33f46e5806cd18a79b7481357b80afcf3
Aleksander <alexlik73@gmail.com>
2022-03-19 14:37:38 +0300

    seve class code

---
+++ main.py
@@ -0,0 +1,57 @@
+import turtle
+import time
+import random
+
+
+def iscollision(obj1, obj2, w1, w2, h1, h2):
+	if obj1.xcor() + w1 > obj2.xcor() - w2 and obj1.xcor() - w1 < obj2.xcor() + w2:
+		if obj1.ycor() + h1 > obj2.ycor() - h2 and obj1.ycor() - h1 < obj2.ycor() + h2:
+			return True
+	return False
+
+def motion(event):
+    x, y = event.x, event.y
+    x = x - 400
+    y = y - 400
+    helper.setposition(x,-y)
+    for hero in heroes:
+	    if hero.grab:
+		    hero.setposition(x,-y)
+
+def click(x,y):
+	for hero in heroes:
+		if iscollision(helper, hero, 20, 20, 20, 20):
+			if hero.grab:
+				hero.grab = False
+				hero.color('black')
+			else:	
+				hero.grab = True
+				hero.color('red')
+
+window = turtle.Screen()
+window.setup(800, 800)
+window.tracer(0)
+
+helper = turtle.Turtle()
+helper.speed(0)
+helper.penup()
+helper.hideturtle()
+
+heroes = []
+for i in range(10):
+	hero = turtle.Turtle()
+	hero.penup()
+	hero.speed(0)
+	hero.shape('square')
+	hero.shapesize(4,4)
+	hero.setposition(random.randint(-400, 400),random.randint(-400, 400))
+	hero.grab = False
+	heroes.append(hero)
+
+window.onclick(click)
+canvas = turtle.getcanvas()
+canvas.bind('<Motion>', motion)
+
+while True:
+	time.sleep(.1)
+	window.update()
