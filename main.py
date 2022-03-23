bfe1fe091d1ce2b052ef082dff170ab3cb438c7e
Aleksander <alexlik73@gmail.com>
2022-03-23 15:35:23 +0300

    add edible

--- main.py
+++ main.py
@@ -23,10 +23,8 @@ def click(x,y):
 		if iscollision(helper, hero, 20, 20, 20, 20):
 			if hero.grab:
 				hero.grab = False
-				hero.color('black')
 			else:	
 				hero.grab = True
-				hero.color('red')
 
 window = turtle.Screen()
 window.setup(800, 800)
@@ -37,15 +35,28 @@ helper.speed(0)
 helper.penup()
 helper.hideturtle()
 
+writer = turtle.Turtle()
+writer.speed(0)
+writer.penup()
+writer.hideturtle()
+
 heroes = []
+edibles = []
+inedibles = []
 for i in range(10):
 	hero = turtle.Turtle()
 	hero.penup()
 	hero.speed(0)
 	hero.shape('square')
 	hero.shapesize(4,4)
+	hero.subject = random.choice(['edible', 'inedible'])
 	hero.setposition(random.randint(-400, 400),random.randint(-400, 400))
 	hero.grab = False
+	if hero.subject == 'edible':
+		hero.color('green')
+		edibles.append(hero)
+	elif hero.subject == 'inedible':
+		inedibles.append(hero)
 	heroes.append(hero)
 
 window.onclick(click)
@@ -53,5 +64,12 @@ canvas = turtle.getcanvas()
 canvas.bind('<Motion>', motion)
 
 while True:
+	if all([edible.xcor() < -100 for edible in edibles]):
+		if all([inedible.xcor() > 100 for inedible in inedibles]):
+			writer.write('Right!', font=('Arial', 68, 'normal'))
+			break
+
 	time.sleep(.1)
 	window.update()
+window.update()
+turtle.done()
